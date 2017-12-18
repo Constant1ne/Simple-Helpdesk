86c30cc62e9811a9e2543d04b0761fb5ca8e94c2
Kup <constantine.ru@gmail.com>
2017-12-18 23:31:41 +0500

    Добавлена модель данных, которая будет хранится в базе

---
+++ Simple Helpdesk/Models/Request.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Simple_Helpdesk.Models
+{
+    /// <summary>
+    /// Класс представляющий заявку
+    /// </summary>
+    public class Request
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual List<RequestDescription> Descriptions { get; set; }
+    }
+
+    /// <summary>
+    /// Класс представляющий состояние заявки
+    /// </summary>
+    public class RequestDescription
+    {
+        public int ID { get; set; }
+        public int RequestID { get; set; }
+        public RequestStatus Status { get; set; }
+        public DateTime ModificationTime { get; set; }
+        public string Descripion { get; set; }
+    }
+
+    public enum RequestStatus
+    {
+        Opened = 1,
+        Solved = 2,
+        Returned = 3,
+        Closed = 4
+    }
+}

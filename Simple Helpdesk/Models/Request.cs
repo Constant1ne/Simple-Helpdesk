2ae65477b8d0d79ee18e155e0c4661073a8c42ab
Kup <constantine.ru@gmail.com>
2017-12-19 00:28:17 +0500

    добавление Entity Framework + контекст модели для отображения в базу

--- Simple Helpdesk/Models/Request.cs
+++ Simple Helpdesk/Models/Request.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Simple_Helpdesk.Models
 {
@@ -11,7 +12,15 @@ namespace Simple_Helpdesk.Models
     public class Request
     {
         public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Название заявки")]
+        [MaxLength(30, ErrorMessage = "Превышена максимальная длина записи")]
         public string Name { get; set; }
+
+        /// <summary>
+        /// Ссылка на описания заявки, отношение one-to-many
+        /// </summary>
         public virtual List<RequestDescription> Descriptions { get; set; }
     }
 
@@ -21,9 +30,28 @@ namespace Simple_Helpdesk.Models
     public class RequestDescription
     {
         public int ID { get; set; }
-        public int RequestID { get; set; }
+
+        /// <summary>
+        /// Внешний ключ, заявка к которой относится данное описание
+        /// </summary>
+        public Request RequestID { get; set; }
+
+        /// <summary>
+        /// Статус заявки 
+        /// </summary>
+        [Display(Name = "Статус")]
         public RequestStatus Status { get; set; }
+
+        /// <summary>
+        /// Дата текущего изменения 
+        /// </summary>
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
         public DateTime ModificationTime { get; set; }
+
+        [Required]
+        [Display(Name = "Описание")]
+        [MaxLength(200, ErrorMessage = "Превышена максимальная длина записи")]
         public string Descripion { get; set; }
     }
 
---
+++ Simple Helpdesk/Models/RequestContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Entity;
+
+namespace Simple_Helpdesk.Models
+{
+    public class RequestContext : DbContext
+    {
+        public RequestContext() : base("RequestsDataBase") { }
+
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<RequestDescription> Descriptions { get; set; }
+    }
+}
